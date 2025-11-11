92d1bee844599f31a3f94a54a423dc326557648f
Shamsuddin <taheeronline@gmail.com>
2025-11-11 19:16:17 +0530

    Add project files.

---
+++ ELTApp/frmGenerateScript.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace ELTApp
+{
+    partial class frmGenerateScript
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGenerateScript));
+            buttonGenerate = new Button();
+            txtInsert = new RichTextBox();
+            checkedListBoxTables = new CheckedListBox();
+            txtFilter = new TextBox();
+            btnLoad = new Button();
+            txtServerName = new TextBox();
+            txtDbName = new TextBox();
+            txtUserName = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            chkTrusted = new CheckBox();
+            btnClear = new Button();
+            lblMessage = new Label();
+            txtSiteFilter = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            chkSelectAll = new CheckBox();
+            SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            buttonGenerate.BackColor = SystemColors.ActiveCaption;
+            buttonGenerate.Font = new Font("Tahoma", 11.25F);
+            buttonGenerate.ForeColor = SystemColors.ButtonHighlight;
+            buttonGenerate.Location = new Point(562, 183);
+            buttonGenerate.Name = "buttonGenerate";
+            buttonGenerate.Size = new Size(132, 31);
+            buttonGenerate.TabIndex = 11;
+            buttonGenerate.Text = "Generate";
+            buttonGenerate.UseVisualStyleBackColor = false;
+            buttonGenerate.Click += buttonGenerate_Click;
+            // 
+            // txtInsert
+            // 
+            txtInsert.BorderStyle = BorderStyle.FixedSingle;
+            txtInsert.Font = new Font("Tahoma", 11.25F);
+            txtInsert.ForeColor = SystemColors.Highlight;
+            txtInsert.Location = new Point(356, 220);
+            txtInsert.Name = "txtInsert";
+            txtInsert.ReadOnly = true;
+            txtInsert.Size = new Size(998, 613);
+            txtInsert.TabIndex = 14;
+            txtInsert.Text = "";
+            // 
+            // checkedListBoxTables
+            // 
+            checkedListBoxTables.BorderStyle = BorderStyle.None;
+            checkedListBoxTables.Font = new Font("Tahoma", 11.25F);
+            checkedListBoxTables.ForeColor = SystemColors.Highlight;
+            checkedListBoxTables.FormattingEnabled = true;
+            checkedListBoxTables.Location = new Point(3, 245);
+            checkedListBoxTables.Name = "checkedListBoxTables";
+            checkedListBoxTables.Size = new Size(338, 588);
+            checkedListBoxTables.Sorted = true;
+            checkedListBoxTables.TabIndex = 13;
+            checkedListBoxTables.ItemCheck += checkedListBoxTables_ItemCheck;
+            // 
+            // txtFilter
+            // 
+            txtFilter.CharacterCasing = CharacterCasing.Upper;
+            txtFilter.Font = new Font("Tahoma", 11.25F);
+            txtFilter.ForeColor = SystemColors.Highlight;
+            txtFilter.Location = new Point(3, 188);
+            txtFilter.Name = "txtFilter";
+            txtFilter.Size = new Size(200, 26);
+            txtFilter.TabIndex = 9;
+            // 
+            // btnLoad
+            // 
+            btnLoad.BackColor = SystemColors.ActiveCaption;
+            btnLoad.Font = new Font("Tahoma", 11.25F);
+            btnLoad.ForeColor = SystemColors.ButtonHighlight;
+            btnLoad.Location = new Point(209, 183);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(132, 31);
+            btnLoad.TabIndex = 10;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = false;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // txtServerName
+            // 
+            txtServerName.CharacterCasing = CharacterCasing.Upper;
+            txtServerName.Font = new Font("Tahoma", 11.25F);
+            txtServerName.ForeColor = SystemColors.Highlight;
+            txtServerName.Location = new Point(129, 23);
+            txtServerName.Name = "txtServerName";
+            txtServerName.Size = new Size(250, 26);
+            txtServerName.TabIndex = 1;
+            // 
+            // txtDbName
+            // 
+            txtDbName.CharacterCasing = CharacterCasing.Upper;
+            txtDbName.Font = new Font("Tahoma", 11.25F);
+            txtDbName.ForeColor = SystemColors.Highlight;
+            txtDbName.Location = new Point(129, 55);
+            txtDbName.Name = "txtDbName";
+            txtDbName.Size = new Size(250, 26);
+            txtDbName.TabIndex = 3;
+            // 
+            // txtUserName
+            // 
+            txtUserName.Font = new Font("Tahoma", 11.25F);
+            txtUserName.ForeColor = SystemColors.Highlight;
+            txtUserName.Location = new Point(129, 87);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(250, 26);
+            txtUserName.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Tahoma", 11.25F);
+            txtPassword.ForeColor = SystemColors.Highlight;
+            txtPassword.Location = new Point(129, 119);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(250, 26);
+            txtPassword.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(7, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 15);
+            label1.TabIndex = 0;
+            label1.Text = "DB Server Name:";
+            label1.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(7, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 15);
+            label2.TabIndex = 2;
+            label2.Text = "DB  Name:";
+            label2.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(7, 93);
+            label3.Name = "label3";
+            label3.Size = new Size(120, 15);
+            label3.TabIndex = 4;
+            label3.Text = "User Name:";
+            label3.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(7, 125);
+            label4.Name = "label4";
+            label4.Size = new Size(120, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Password:";
+            label4.TextAlign = ContentAlignment.TopRight;
+            // 
+            // chkTrusted
+            // 
+            chkTrusted.AutoSize = true;
+            chkTrusted.CheckAlign = ContentAlignment.MiddleRight;
+            chkTrusted.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            chkTrusted.ForeColor = SystemColors.ButtonHighlight;
+            chkTrusted.Location = new Point(129, 151);
+            chkTrusted.Name = "chkTrusted";
+            chkTrusted.Size = new Size(157, 20);
+            chkTrusted.TabIndex = 8;
+            chkTrusted.Text = "Trusted Connection:";
+            chkTrusted.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = SystemColors.ActiveCaption;
+            btnClear.Font = new Font("Tahoma", 11.25F);
+            btnClear.ForeColor = SystemColors.ButtonHighlight;
+            btnClear.Location = new Point(1222, 185);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(132, 31);
+            btnClear.TabIndex = 12;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+            // 
+            // lblMessage
+            // 
+            lblMessage.Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblMessage.ForeColor = SystemColors.ButtonHighlight;
+            lblMessage.Location = new Point(700, 153);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(516, 63);
+            lblMessage.TabIndex = 15;
+            lblMessage.Text = "Status";
+            // 
+            // txtSiteFilter
+            // 
+            txtSiteFilter.CharacterCasing = CharacterCasing.Upper;
+            txtSiteFilter.Font = new Font("Tahoma", 11.25F);
+            txtSiteFilter.ForeColor = SystemColors.Highlight;
+            txtSiteFilter.Location = new Point(356, 188);
+            txtSiteFilter.Name = "txtSiteFilter";
+            txtSiteFilter.Size = new Size(200, 26);
+            txtSiteFilter.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(356, 170);
+            label5.Name = "label5";
+            label5.Size = new Size(120, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Site Filter:";
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(3, 170);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 15);
+            label6.TabIndex = 4;
+            label6.Text = "Table Filter:";
+            // 
+            // chkSelectAll
+            // 
+            chkSelectAll.AutoSize = true;
+            chkSelectAll.CheckAlign = ContentAlignment.MiddleRight;
+            chkSelectAll.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            chkSelectAll.ForeColor = SystemColors.ButtonHighlight;
+            chkSelectAll.Location = new Point(3, 220);
+            chkSelectAll.Name = "chkSelectAll";
+            chkSelectAll.Size = new Size(91, 20);
+            chkSelectAll.TabIndex = 8;
+            chkSelectAll.Text = "Select All:";
+            chkSelectAll.UseVisualStyleBackColor = true;
+            chkSelectAll.CheckedChanged += chkSelectAll_CheckedChanged;
+            // 
+            // frmGenerateScript
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1368, 847);
+            Controls.Add(chkSelectAll);
+            Controls.Add(chkTrusted);
+            Controls.Add(lblMessage);
+            Controls.Add(label4);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Controls.Add(txtDbName);
+            Controls.Add(txtServerName);
+            Controls.Add(btnLoad);
+            Controls.Add(txtSiteFilter);
+            Controls.Add(txtFilter);
+            Controls.Add(checkedListBoxTables);
+            Controls.Add(txtInsert);
+            Controls.Add(btnClear);
+            Controls.Add(buttonGenerate);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmGenerateScript";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Generate Scripts";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonGenerate;
+        private RichTextBox txtInsert;
+        private CheckedListBox checkedListBoxTables;
+        private TextBox txtFilter;
+        private Button btnLoad;
+        private TextBox txtServerName;
+        private TextBox txtDbName;
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private CheckBox chkTrusted;
+        private Button btnClear;
+        private Label lblMessage;
+        private TextBox txtSiteFilter;
+        private Label label5;
+        private Label label6;
+        private CheckBox chkSelectAll;
+    }
+}
---
+++ ELTApp/frmGenerateScript.cs
@@ -0,0 +1,288 @@
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace ELTApp
+{
+    public partial class frmGenerateScript : Form
+    {
+        public string connectionString;// = "Server=IE-0024\\SQLEXPRESS;Database=VAL_EM;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public frmGenerateScript()
+        {
+            InitializeComponent();
+        }
+
+        private string BuildConnectionString()
+        {
+            string server = txtServerName.Text.Trim();
+            string database = txtDbName.Text.Trim();
+            string user = txtUserName.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+
+            if (string.IsNullOrWhiteSpace(server) || string.IsNullOrWhiteSpace(database))
+            {
+                MessageBox.Show("Please enter both Server Name and Database Name.", "Missing Info",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return null;
+            }
+
+            // Use either Trusted Connection or SQL Authentication
+            if (chkTrusted.Checked)
+            {
+                return $"Server={server};Database={database};Trusted_Connection=True;TrustServerCertificate=True;";
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(password))
+                {
+                    MessageBox.Show("Please enter both User Name and Password for SQL Authentication.", "Missing Info",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return null;
+                }
+
+                return $"Server={server};Database={database};User ID={user};Password={password};TrustServerCertificate=True;";
+            }
+        }
+
+        private void LoadTableNames(string query)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        // If textbox is empty, use wildcard to show all tables
+                        string filterValue = string.IsNullOrWhiteSpace(txtFilter.Text) ? "%" : $"%{txtFilter.Text.Trim()}%";
+
+                        cmd.Parameters.AddWithValue("@filter", filterValue);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            checkedListBoxTables.Items.Clear();
+                            while (reader.Read())
+                            {
+                                checkedListBoxTables.Items.Add(reader.GetString(0).ToUpper());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load tables: {ex.Message}");
+            }
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedTables = new List<string>();
+
+                // Loop through all checked items
+                foreach (var item in checkedListBoxTables.CheckedItems)
+                {
+                    string tableName = item.ToString();
+
+                    // Check if this is a "Site" table
+                    if (tableName.Contains("Site", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Generate base table name (remove "Site")
+                        string baseTableName = tableName.Replace("Site", "", StringComparison.OrdinalIgnoreCase)
+                                                        .Replace("__", "_") // clean double underscores if any
+                                                        .TrimEnd('_');      // remove trailing underscore if exists
+
+                        // Add the base table first (even if not selected)
+                        if (!selectedTables.Contains(baseTableName, StringComparer.OrdinalIgnoreCase))
+                            selectedTables.Add(baseTableName);
+
+                        // Then add the Site table
+                        if (!selectedTables.Contains(tableName, StringComparer.OrdinalIgnoreCase))
+                            selectedTables.Add(tableName);
+                    }
+                    else
+                    {
+                        // Non-site table: just add it normally
+                        if (!selectedTables.Contains(tableName, StringComparer.OrdinalIgnoreCase))
+                            selectedTables.Add(tableName);
+
+                        // Try to find a corresponding "Site" table in the selection
+                        string siteVariant = tableName.Insert(tableName.LastIndexOf('_') + 1, "Site_");
+
+                        foreach (var chkItem in checkedListBoxTables.CheckedItems)
+                        {
+                            string chkName = chkItem.ToString();
+                            if (chkName.Equals(siteVariant, StringComparison.OrdinalIgnoreCase))
+                            {
+                                if (!selectedTables.Contains(chkName, StringComparer.OrdinalIgnoreCase))
+                                    selectedTables.Add(chkName);
+                            }
+                        }
+                    }
+                }
+
+
+
+                if (selectedTables.Count == 0)
+                {
+                    MessageBox.Show("Please select at least one table.");
+                    return;
+                }
+
+                StringBuilder allInserts = new StringBuilder();
+
+                foreach (var table in selectedTables)
+                {
+                    allInserts.AppendLine($"-- ==== INSERTS FOR TABLE: {table} ====");
+                    string script = GenerateInsertStatements(connectionString, table, txtSiteFilter.Text);
+                    allInserts.AppendLine(script);
+                    allInserts.AppendLine();
+                }
+
+                txtInsert.Text = allInserts.ToString();
+
+                string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "GeneratedSQL"
+);
+                Directory.CreateDirectory(folderPath); // creates if missing
+
+                string filePath = Path.Combine(folderPath, "InsertScripts.sql");
+                File.WriteAllText(filePath, allInserts.ToString());
+
+                lblMessage.Text = $"Successfully Saved to :\n{filePath}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        static string GenerateInsertStatements(string conStr, string tableName, string siteName = "")
+        {
+            StringBuilder sb = new StringBuilder();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(conStr))
+                {
+                    conn.Open();
+
+                    string query = $"SELECT * FROM {tableName}";
+                    if (tableName.Contains("SITE", StringComparison.OrdinalIgnoreCase))
+                    {
+                        query = $@"
+                    SELECT ID_LIST, ID_SITE 
+                    FROM {tableName} 
+                    WHERE ID_SITE = (
+                        SELECT ID FROM EM_SITES WHERE SITENAME = @SiteName
+                    )";
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        if (tableName.Contains("SITE", StringComparison.OrdinalIgnoreCase))
+                            cmd.Parameters.AddWithValue("@SiteName", siteName);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            var schema = reader.GetColumnSchema();
+                            var columnNames = new List<string>();
+                            foreach (var col in schema)
+                                columnNames.Add(col.ColumnName);
+
+                            if (!reader.HasRows)
+                            {
+                                sb.AppendLine($"-- Table {tableName} has no data.");
+                                return sb.ToString();
+                            }
+
+                            while (reader.Read())
+                            {
+                                var values = new List<string>();
+
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    object value = reader.GetValue(i);
+
+                                    if (value == DBNull.Value)
+                                        values.Add("NULL");
+                                    else if (value is string || value is DateTime || value is Guid)
+                                    {
+                                        string escaped = value.ToString().Replace("'", "''");
+                                        values.Add($"'{escaped}'");
+                                    }
+                                    else if (value is bool)
+                                        values.Add((bool)value ? "1" : "0");
+                                    else if (value is byte[])
+                                    {
+                                        byte[] bytes = (byte[])value;
+                                        string hex = BitConverter.ToString(bytes).Replace("-", "");
+                                        values.Add($"0x{hex}");
+                                    }
+                                    else
+                                        values.Add(value.ToString());
+                                }
+
+                                string insert = $"INSERT INTO {tableName} ({string.Join(",", columnNames)}) VALUES ({string.Join(", ", values)});";
+                                sb.AppendLine(insert.ToUpper());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Return a detailed error message including table name
+                string errorMessage = $"-- ERROR in table '{tableName}': {ex.Message}";
+                return errorMessage;
+            }
+
+            return sb.ToString();
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string query = @"
+                        SELECT TABLE_NAME 
+                        FROM INFORMATION_SCHEMA.TABLES 
+                        WHERE TABLE_TYPE = 'BASE TABLE'
+                        and TABLE_NAME like @filter
+                        ORDER BY TABLE_NAME;";
+            connectionString = BuildConnectionString();
+            if (connectionString == null || connectionString.Length == 0)
+                return;
+            LoadTableNames(query);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInsert.Clear();
+        }
+
+        private void chkSelectAll_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isChecked = chkSelectAll.Checked;
+
+            // Loop through all items in the CheckedListBox
+            for (int i = 0; i < checkedListBoxTables.Items.Count; i++)
+            {
+                checkedListBoxTables.SetItemChecked(i, isChecked);
+            }
+        }
+
+        private void checkedListBoxTables_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            // Delay evaluation until after the item is checked/unchecked
+            this.BeginInvoke((Action)(() =>
+            {
+                bool allChecked = checkedListBoxTables.CheckedItems.Count == checkedListBoxTables.Items.Count;
+                chkSelectAll.Checked = allChecked;
+            }));
+        }
+    }
+}
